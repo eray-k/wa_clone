09dc58fd1215d6044338afcbc13f3273702024a5
300Spartian <47210403+eray-k@users.noreply.github.com>
2023-07-16 20:34:52 +0300

    tab bar and ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
@@ -24,22 +25,83 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  TabBar get _tabBar => const TabBar(
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white,
+        indicatorColor: Colors.white,
+        indicatorSize: TabBarIndicatorSize.tab,
+        tabs: [
+          Tab(text: 'Contacts'),
+          Tab(text: 'Status'),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Messages'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return const ContactCard();
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.message_rounded),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'MESSAGES',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.camera_alt,
+                  color: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert, color: Colors.white)),
+          ],
+          bottom: PreferredSize(
+              preferredSize: _tabBar.preferredSize,
+              child: Material(
+                color: Theme.of(context).colorScheme.primary,
+                child: Theme(
+                    data: ThemeData().copyWith(splashColor: Colors.white10),
+                    child: _tabBar),
+              )),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: TabBarView(children: [
+                ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return const ContactCard();
+                  },
+                ),
+                ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return const ContactCard();
+                  },
+                ),
+              ]),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: const CircleBorder(),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          child: const Icon(
+            Icons.message_rounded,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
